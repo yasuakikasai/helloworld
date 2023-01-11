27e905af151f4eb31b69bb5c947d3b4d6fea1243
yasuakikasai <35493385+yasuakikasai@users.noreply.github.com>
2023-01-11 21:31:00 +0900

    Add files via upload

--- streamlit_st_slider.py
+++ streamlit_st_slider.py
@@ -55,3 +55,28 @@ option = st.selectbox(
      ('Blue', 'Red', 'Green'))
 
 st.write('Your favorite color is ', option)
+
+
+st.header('st.multiselect')
+options = st.multiselect(
+     'What are your favorite colors',
+     ['Green', 'Yellow', 'Red', 'Blue'],
+     ['Yellow', 'Red'])
+
+st.write('You selected:', options)
+
+
+st.header('st.checkbox')
+st.write ('What would you like to order?')
+icecream = st.checkbox('Ice cream')
+coffee = st.checkbox('Coffee')
+cola = st.checkbox('Cola')
+
+if icecream:
+     st.write("Great! Here's some more 🍦")
+
+if coffee: 
+     st.write("Okay, here's some coffee ☕")
+
+if cola:
+     st.write("Here you go 🥤")
