ecb5ed5b4b6055530e83e7e9c4fd2d3fc855c572
yasuakikasai <y.kasai6155@gmail.com>
2023-01-15 10:14:46 +0000

    add line

--- Streamlit Components.py
+++ Streamlit Components.py
@@ -1,9 +1,11 @@
 import streamlit as st
-import altair as alt
 import pandas as pd
-import numpy as np
+import pandas_profiling
+from streamlit_pandas_profiling import st_profile_report
 
-st.write('Hello world!')
-st.header('st.write')
+st.header('`streamlit_pandas_profiling`')
 
-st.write('Hello world!')
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+
+pr = df.profile_report()
+st_profile_report(pr)
