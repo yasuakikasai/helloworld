1a2ed014f3e737fca5db3e3e4bd7c4b908ce235b
yasuakikasai <y.kasai6155@gmail.com>
2023-01-17 12:21:43 +0000

    st.progress

--- streamlit_uploadcsv.py
+++ streamlit_uploadcsv.py
@@ -14,3 +14,19 @@ if uploaded_file is not None:
   st.write(df.describe())
 else:
   st.info('☝️ Upload a CSV file')
+
+
+import time
+
+st.title('st.progress')
+
+with st.expander('About this app'):
+     st.write('You can now display the progress of your calculations in a Streamlit app with the `st.progress` command.')
+
+my_bar = st.progress(0)
+
+for percent_complete in range(100):
+     time.sleep(0.05)
+     my_bar.progress(percent_complete + 1)
+
+st.balloons()
